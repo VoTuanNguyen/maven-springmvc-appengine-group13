6dc4d5b8c3b45f312abe71707533799edb1505be
VoTuanNguyen <14110133@student.hcmute.edu.vn>
2017-05-08 00:13:21 +0700

    update

--- src/main/java/com/hellokoding/springboot/WebApplication.java
+++ src/main/java/com/hellokoding/springboot/WebApplication.java
@@ -1,5 +1,11 @@
 package com.hellokoding.springboot;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -15,5 +21,40 @@ public class WebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(WebApplication.class, args);
     }
+    Connection connect = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+
+	protected void hasDriver() throws Exception {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException ex) {
+			throw new Exception("Invalid Driver!!Please check this drver....");
+		}
+	}
+
+	public Connection openConnect() throws Exception {
+		if (connect == null) {
+			hasDriver();
+			String url = "jdbc:mysql://localhost/ckeditor?useUnicode=true&characterEncoding=utf-8";
+			// url_sql = "....";
+			try {
+				this.connect = DriverManager.getConnection(url, "root",
+						"123456");
+			} catch (SQLException e) {
+				throw new Exception(e.getMessage()
+						+ "Connect failed to database .... ");
+			}
+		}
+		return connect;
+	}
+	public void closeConnet() throws SQLException {
+		if (rs != null && !rs.isClosed())
+			rs.close();
+		if (stmt != null)
+			stmt.close();
+		if (connect != null)
+			connect.close();
+	}
 }
 
--- src/main/webapp/WEB-INF/tags/edit.tag
+++ src/main/webapp/WEB-INF/tags/edit.tag
@@ -1,6 +1,6 @@
 <%@tag description="Tag Edit" pageEncoding="UTF-8"%>
 <%@tag import="java.sql.ResultSet"%>
-<%@tag import="com.hellokoding.springboot.ConnectDB"%>
+<%@tag import="com.hellokoding.springboot.WebApplication"%>
 <html>
 <body>
 	<script src="ckeditor/ckeditor.js"></script>
@@ -8,9 +8,9 @@
 		<form action="" method="post">
 			<!-- (2): textarea sẽ được thay thế bởi CKEditor -->
 			<textarea id="editor1" name="editor1" cols="80" rows="10">
-			<%-- <%
+			<%
 			ResultSet rs = null;
-	    	ConnectDB DB = new ConnectDB();
+	    	WebApplication DB = new WebApplication();
 			String strSQL = "select * from edit";
 			try {
 				 rs = DB.openConnect().createStatement().executeQuery(strSQL);
@@ -22,7 +22,7 @@
 			} catch (Exception e) {
 				throw new Exception(e.getMessage() + " Error at : " + strSQL);
 			}
-			%> --%>
+			%> 
 			<p>${id1} - <strong>${ten1}</strong></p>
 			<p>${id2} - <strong>${ten2}</strong></p>
 			<p>${id3} - <strong>${ten3}</strong></p>
